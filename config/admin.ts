d5e6e2488e42e8f3c8a8f87d71940cc413f2e1e3
Yeider Adrian Mina Caicedo <yeideradrian.nobaqgmail.com>
2024-11-16 21:10:56 -0500

    Configuracion

--- config/admin.ts
+++ config/admin.ts
@@ -1,9 +1,9 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET','4b9191a735ed9b7a31289fca85ac48b688089b4896dfa72654cd7802b3f64a01'),
+    secret: env('ADMIN_JWT_SECRET',),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT','9wutl0MqlVDMHGeMhk3GKA=='),
+    salt: env('API_TOKEN_SALT'),
   },
   transfer: {
     token: {
--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,6 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS',['48pDqw1PlMzH7k8xsNndD8vPmvbybNrO','8lsZXIOaqNCjC3mU7gpvT2J3HK6DHTon']),
+    keys: env.array('APP_KEYS'),
   },
 });
